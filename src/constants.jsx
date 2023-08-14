22a9b5a7164cb9981fd42239879f72f66aa02271
Akabhi0 <abhishekabhiabhi0@gmail.com>
2023-08-14 13:38:09 +0400

    git testing

---
+++ src/constants.jsx
@@ -0,0 +1,45 @@
+export const itemList = {
+  nav_itme: [
+    { name: "Home", to: "/" },
+    { name: "About Us", to: "/about" },
+    { name: "Contact Us", to: "/contact" },
+    { name: "Cart", to: "/cart" },
+  ],
+  restrocard_item: [
+    {
+      url: "https://thumbs.dreamstime.com/b/cozy-restaurant-tables-ready-dinner-39875776.jpg",
+      title: "Bruno Bar",
+      rating: 4,
+      deliveryTime: "24 min",
+      cusine: "Non-veg,contenental,veg",
+    },
+    {
+      url: "https://thumbs.dreamstime.com/b/cozy-restaurant-tables-ready-dinner-39875776.jpg",
+      title: "Bruno Bar",
+      rating: 4.23,
+      deliveryTime: "24 min",
+      cusine: "Non-veg,contenental,veg",
+    },
+    {
+      url: "https://thumbs.dreamstime.com/b/cozy-restaurant-tables-ready-dinner-39875776.jpg",
+      title: "Bruno Bar",
+      rating: 4.45,
+      deliveryTime: "24 min",
+      cusine: "Non-veg,contenental,veg",
+    },
+    {
+      url: "https://thumbs.dreamstime.com/b/cozy-restaurant-tables-ready-dinner-39875776.jpg",
+      title: "Bruno Bar",
+      rating: 2.3,
+      deliveryTime: "24 min",
+      cusine: "Non-veg,contenental,veg",
+    },
+  ]
+};
+
+export const LOGO_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId="
+
+// export {
+//   itemList,
+//   resUrl
+// }
